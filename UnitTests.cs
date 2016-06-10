4ae5b21ac26a40ffcb77bb55a3a74e677934a304
Dan Leeaphon <dleeapho@microsoft.com>
2016-06-10 19:51:00 +0000

    Simple dotnet test

---
+++ UnitTests.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+namespace UnitTests
+{
+	public class Tests
+	{
+		[Fact]
+		public void Test1()
+		{ Assert.Equal(0, 1); }
+
+		[Fact]
+		public void Test2()
+		{ Assert.Equal(1, 1); }
+	}
+}
